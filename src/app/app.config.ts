0129c12a627a9f438f1e53773b1771265b8bd2eb
dany1468 <dany1468@gmail.com>
2023-08-08 15:08:15 +0900

    Set up to use firebase emulator

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,43 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions, connectFunctionsEmulator } from '@angular/fire/functions';
+import { getAuth, provideAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { getStorage, provideStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { environment } from 'src/environments/environment';
+
+const provideFirebase = () => importProvidersFrom([
+  provideFirebaseApp(() => initializeApp(environment.firebase)),
+  provideAuth(() => {
+    const auth = getAuth();
+    if (environment.useEmulators) {
+      connectAuthEmulator(auth, 'http://localhost:9099');
+    }
+    return auth;
+  }),
+  provideFirestore(() => {
+    const firestore = getFirestore();
+    if (environment.useEmulators) {
+      connectFirestoreEmulator(firestore, 'localhost', 8080);
+    }
+    return firestore;
+  }),
+  provideFunctions(() => {
+    const functions = getFunctions();
+    if (environment.useEmulators) {
+      connectFunctionsEmulator(functions, 'localhost', 5001);
+    }
+    return functions;
+  }),
+  provideStorage(() => {
+    const storage = getStorage();
+    if (environment.useEmulators) {
+      connectStorageEmulator(storage, 'localhost', 9199);
+    }
+    return storage;
+  }),
+]);
 
 export const appConfig: ApplicationConfig = {
-  providers: []
+  providers: [provideFirebase()]
 };
